041a853ea42d632e3ab33a033ef2a13a0ba5756c
Harish Sinha <hsinha76@gmail.com>
2021-04-22 22:27:51 +0530

    Update InterfaceAPI.java

--- app/src/main/java/com/hsdroid/moviebuff/Api/InterfaceAPI.java
+++ app/src/main/java/com/hsdroid/moviebuff/Api/InterfaceAPI.java
@@ -7,7 +7,7 @@ import retrofit2.http.GET;
 
 public interface InterfaceAPI {
 
-    String api_key = "6281dfd9c4c5cc256e22103a83619be0";
+    String api_key = "YOUR_API_KEY_HERE";
 
     @GET("popular?api_key=" + api_key)
     Call<JsonObject> getMovie();
